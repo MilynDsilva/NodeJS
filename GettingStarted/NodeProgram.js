6785518172af901cc74f8f493ec08dbc0856127c
Milyn <milyng.dsilva@gmail.com>
2021-11-30 17:10:57 +0530

    commit

---
+++ GettingStarted/NodeProgram.js
@@ -0,0 +1,16 @@
+function name1(name){
+    console.log('Hey '+name);
+}
+
+
+//console.log(window);
+name1('Jimmy')
+
+
+//Asynchrnous = non blocking , synchronus = blocking
+
+// K1 Thread (single thread) performs both task without wating one to finish in 
+// Asynchronus
+
+
+//Reverse in synchrnous (Waits for one to finish)

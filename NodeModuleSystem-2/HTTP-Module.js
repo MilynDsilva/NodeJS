2d265dfcb9c38696a98978cc401a1d755d7d7638
Milyn <milyng.dsilva@gmail.com>
2021-12-03 10:36:38 +0530

    -

--- NodeModuleSystem-2/HTTP-Module.js
+++ NodeModuleSystem-2/HTTP-Module.js
@@ -1,4 +1,54 @@
+const http = require('http');
 
+const server = http.createServer((req, res)=> {
+    if(req.url === '/') {
+        res.write('hello world');
+        res.end();
+    }
+
+    if(req.url === '/api/cs'){
+        res.write(JSON.stringify([1,2,3]));
+        res.end();
+    }
+}); //server is event emitter
+
+
+
+server.listen(3000);
+
+console.log('Listening on port 3000...');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//BASIC SERVER \/
+
+
+// const http = require('http');
+
+// const server = http.createServer(); //server is event emitter
+
+// server.on('connection',(socket) =>{
+//     console.log('New connection...');
+// });
+
+// server.listen(3000);
+
+// console.log('Listening on port 3000...');
 
 
 
